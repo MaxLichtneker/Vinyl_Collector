ababb61d082f1dbd22bb020c12db27879ddf9e32
MaxLichtneker <maxwil2001@hotmail.com>
2025-12-09 18:50:18 +0100

    changed some colors

--- Vinyl-Collector/src/components/collection/Item.jsx
+++ Vinyl-Collector/src/components/collection/Item.jsx
@@ -3,7 +3,7 @@ import "./Item.css"
 function Item(props){
     return(
         <div className="item-container">
-            <div style={{width:"300px", height:"auto",margin:"10px", backgroundColor:"#727272"}}/>
+            <div style={{width:"300px", height:"auto",margin:"10px", backgroundColor:"#e0e0e0ff"}}/>
             <div className="text-container">
                 <p>{props.albumName}</p>
                 <p>{props.artistName}</p>
